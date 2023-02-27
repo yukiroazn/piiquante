7e18554602f751d53bc64c6608c2da0641845762
yukiroazn <yukiro.azn@live.com>
2023-02-27 12:02:49 +0100

    Update
    Adding mongo sanitize
    Log in Limitations
    File size limitation

--- back/index.js
+++ back/index.js
@@ -1,18 +1,32 @@
-const { app, express } = require("./server") // Pour créer des applis web avec Node
-const helmet = require('helmet') // Pour sécuriser les en-tête http de l'application express
-const port = 3000
-const path = require("path") // Pour pouvoir travailler avec les chemins des fichiers
+const { app, express } = require("./server"); // Pour créer des applis web avec Node
+const helmet = require('helmet'); // Pour sécuriser les en-tête http de l'application express
+const port = 3000;
+const path = require("path"); // Pour pouvoir travailler avec les chemins des fichiers
 
 // Connection to Database
-require("./models/users")
+require("./models/users");
 
 // Importation controllers for users and sauces
-const { router } = require("./routes/sauce")
-const { users } = require("./routes/users")
+const { router } = require("./routes/sauce");
+const { users } = require("./routes/users");
+
+// Middleware
+const mongoSanitize = require('express-mongo-sanitize'); // To sanitize user input for MongoDB queries
+const rateLimit = require("express-rate-limit"); // To rate limit requests
+
+// Limit the rate of requests made to the API to 100 requests per 15 minutes
+const limiter = rateLimit({
+windowMs: 15 * 60 * 1000, // 15 minutes
+max: 100, // limit each IP to 100 requests per windowMs
+message: "Too many requests from this IP, please try again in 15 minutes"
+})
 
 // Routes
-app.use("/api/sauces", router) // Api pour les sauces
-app.use("/api/auth", users) // Api authorizer les utilisateurs
-app.use("/images", express.static(path.join(__dirname, 'images'))) // Pour servir des fichiers statiques tels que des images, des fichiers CSS et des fichiers JS.
-app.listen(port, () => console.log('listening on port' + port)) // pour lancer un serveur Web
-app.use(helmet()) // Je protège l'appli de certaines vulnerabilités en protégeant les en-têtes
+app.use(mongoSanitize()); // Sanitize user input to prevent MongoDB injection attacks
+app.use(limiter); // Apply rate limiting to all routes
+app.use("/api/sauces", router); // Api pour les sauces
+app.use("/api/auth", users); // Api authorizer les utilisateurs
+app.use("/images", express.static(path.join(__dirname, 'images'))); // Pour servir des fichiers statiques tels que des images, des fichiers CSS et des fichiers JS.
+app.use(helmet()); // Je protège l'appli de certaines vulnerabilités en protégeant les en-têtes
+
+app.listen(port, () => console.log('listening on port' + port)); // pour lancer un serveur Web
--- back/middleware/multer.js
+++ back/middleware/multer.js
@@ -4,22 +4,30 @@ const multer = require("multer");
 
 // Générer le format des images
 const imageFilter = function(req, file, cb) {
-// J'authorise les types de fichier pour générer les extensions possibles
-if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|webp)$/)) {
-req.fileValidationError = 'Only image files are allowed!';
-return cb(new Error('Only image files are allowed!'), false);
+if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|webp)$/)) { // J'authorise les types de fichier pour générer les extensions possibles
+req.fileValidationError = 'Only image files are allowed!'
+return cb(new Error('Only image files are allowed JPG,PNG,WEBP!'), false)
 }
 cb(null, true)
 }
 
 // On enregistre les images téléchargées par le user dans le disc
-const storage = multer.diskStorage({destination: "images/", // La destination
-filename: function(req, file, cb) { // Renvoie en callback le nom du fichier final
-cb(null, Date.now() + "-" + file.originalname) // Avec la date + nom de fichier
+const storage = multer.diskStorage({
+destination: "images/", // La destination
+filename: function(req, file, cb) {
+// Renvoie en callback le nom du fichier final
+cb(null, Date.now() + "-" + file.originalname)
 }
 })
 
-// J'exporte multer en appelant le module storage
-const upload = multer({ storage: storage, fileFilter: imageFilter })
+// On crée une constante pour stocker les options de Multer
+const uploadOptions = 
+{
+storage: storage,
+fileFilter: imageFilter,
+limits: { fileSize: 5 * 1024 * 1024 } // Limite à 5MB
+}
+
+const upload = multer(uploadOptions)
 
 module.exports = { upload }
